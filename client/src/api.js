caa92519921777f34468acd7b501ad1ff8d2f4c7
SUMIT <sumit.4814802723@cse.mait.ac.in>
2026-02-20 17:01:40 +0530

    force redeploy

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL:  "https://ai-expenses-tracker.onrender.com/api",
+  baseURL: "https://ai-expenses-tracker.onrender.com/api"
 });
 
 API.interceptors.request.use((req) => {
