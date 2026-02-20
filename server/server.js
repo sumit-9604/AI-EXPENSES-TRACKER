631ee324e45f185003781f1395fab76fe4c94c42
SUMIT <sumit.4814802723@cse.mait.ac.in>
2026-02-20 16:23:44 +0530

    CORS fix

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const app = express();
 
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://ai-expenses-tracker.onrender.com",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
